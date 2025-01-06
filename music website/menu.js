9ce949fa7fb98cb60421d5ec8afc56b72af561ae
ShyamK-Dev <shyamkamila174@gmail.com>
2025-01-06 12:27:18 +0530

    Add mobile menu functionality and styles

---
+++ music website/menu.js
@@ -0,0 +1,10 @@
+const menuBarEl = document.getElementById("menuBar");
+const menuMobileEl = document.querySelector(".menu-mobile");
+
+const onClose = () => {
+    menuMobileEl.classList.toggle("show-menu");
+};
+
+menuBarEl.addEventListener("click", () => {
+    onClose();
+});
